b38b11bd9187f7eebae96180c644fddadbb0fd05
James Pittendreigh <james.pittendreigh@skybettingandgaming.com>
2020-05-16 08:24:59 +0100

    Super hacky string manipulation version of nested data class diff:
    data class diff for com.sksamuel.kotest.matchers.Baz
    ├ booFoo: data class diff for com.sksamuel.kotest.matchers.BooFoo
    │  ├ a: expected:<9L> but was:<8L>
    │  └ foo: data class diff for com.sksamuel.kotest.matchers.Foo
    │     ├ a: expected:<123L> but was:<321L>
    │     ├ b: expected:<"a"> but was:<"b">
    │     └ c: expected:<"a"> but was:<"c">
    ├ i: expected:<66> but was:<67>
    └ listOf: expected:<["cheese", 22]> but was:<["egg", 1]>

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/DataClassEq.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/DataClassEq.kt
@@ -4,9 +4,13 @@ import io.kotest.assertions.Actual
 import io.kotest.assertions.Expected
 import io.kotest.assertions.failure
 import io.kotest.assertions.show.show
+import io.kotest.matchers.shouldBe
 import kotlin.reflect.KProperty1
 
 internal object DataClassEq : Eq<Any> {
+
+   private const val dataClassDiffText = "data class diff for "
+
    override fun equals(actual: Any, expected: Any): Throwable? {
       return if (test(actual, expected)) null else {
          failure(Expected(expected.show()), Actual(actual.show()), dataClassDiff(actual, expected))
@@ -23,17 +27,35 @@ internal object DataClassEq : Eq<Any> {
 
          // https://discuss.kotlinlang.org/t/type-projection-clash-when-accessing-property-delegate-instance/8331
          // https://youtrack.jetbrains.com/issue/KT-16432?_ga=2.265298440.640424854.1589134567-1685779670.1523969764
-         propsInEquals.filter {prop ->
+         val filtered = propsInEquals.map { prop ->
             val nuProp = (prop as KProperty1<Any, *>)
-            nuProp(actual) != nuProp(expected)
-         }.flatMap{
-            val fieldName = it.name.split('.').last()
-            val nuProp = (it as KProperty1<Any, *>)
-            listOf(" - field $fieldName: " + nuProp(actual).show().value + " did not equal " + nuProp(expected).show().value )
-         }.joinToString(separator = "\n", prefix = "Diff in ${actualClass.simpleName}:\n", postfix = "\n")
+            val failMsg: String? = runCatching { nuProp(actual) shouldBe nuProp(expected) }
+               .fold(onSuccess = { null }, onFailure = { it.message })
+            Pair(nuProp, failMsg)
+         }.filter{ it.second != null }
+
+         val filteredSize = filtered.size
+         filtered.mapIndexed {index, (property, failMsg) ->
+            val fieldName = property.name.split('.').last()
+            val isLastChar = (index +1) == filteredSize
+            val startChar = if (isLastChar) '└' else '├'
+            "$startChar $fieldName: ${formatDataClassDiff(failMsg, isLastChar)}"
+         }.joinToString(separator = "\n", prefix = dataClassDiffText + actualClass.qualifiedName + "\n", postfix = "\n\n")
       } else {
          ""
       }
+
+   private fun formatDataClassDiff(failMsg: String?, isLast: Boolean): String {
+      val startChar = if (isLast) ' ' else '│'
+      return failMsg?.run {
+         if (startsWith(dataClassDiffText)) {
+            lines()
+               .mapIndexed { lineNo, line -> if (lineNo == 0) line else "$startChar  " + line }
+               .dropLast(2)
+               .joinToString(separator = "\n", prefix = "")
+         } else this
+      } ?: ""
+   }
 
    private fun test(a: Any?, b: Any?): Boolean {
       return makeComparable(a) == makeComparable(b)
